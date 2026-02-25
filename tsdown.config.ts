c145ae73a4918d7f5db82e8d00ad9d9e1683f2d2
Azir-11 <2075125282@qq.com>
2026-02-25 15:29:13 +0800

    fix: 更新 package.json 版本和导出配置，优化 tsdown 配置以支持多种输出格式

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,10 +1,44 @@
 import { defineConfig } from 'tsdown'
 
-export default defineConfig({
-  entry: './src/index.ts',
-  outDir: './dist',
-  format: ['cjs', 'esm'],
-  dts: './dist/index.d.ts',
-  clean: true,
-  sourcemap: true,
-})
+export default defineConfig([
+  {
+    entry: './src/index.ts',
+    outDir: './dist',
+    format: ['cjs', 'esm'],
+    dts: true,
+    clean: true,
+    sourcemap: true,
+  },
+  {
+    entry: { arcdash: './src/index.ts' },
+    outDir: './cdn',
+    format: ['iife'],
+    outExtensions: () => ({ js: '.js' }),
+    globalName: 'arcdash',
+    dts: false,
+    clean: true,
+    sourcemap: false,
+    minify: false,
+  },
+  {
+    entry: { arcdash: './src/index.ts' },
+    outDir: './cdn',
+    format: ['esm'],
+    outExtensions: () => ({ js: '.esm.js' }),
+    dts: false,
+    clean: false,
+    sourcemap: false,
+    minify: false,
+  },
+  {
+    entry: { arcdash: './src/index.ts' },
+    outDir: './cdn',
+    format: ['iife'],
+    outExtensions: () => ({ js: '.min.js' }),
+    globalName: 'arcdash',
+    dts: false,
+    clean: false,
+    sourcemap: false,
+    minify: true,
+  },
+])
